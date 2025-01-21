8ce185d271e35ddfc9da0fd4e28b52f42decbcae
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-01-22 02:38:30 +0500

    added Header, Hero at Home and fixed some bags

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,26 +4,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { increment } from "@/redux/features/counter-slice";
 import { useGetProductsQuery } from "@/redux/api/product-api";
 import { IProduct } from "@/types";
+import Hero from "./Hero";
 
 const Home = () => {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
-  const { data } = useGetProductsQuery({order:"desc"});
+  const { data } = useGetProductsQuery({ order: "desc" });
   console.log(data?.data?.products);
 
   return (
     <div>
+      <Hero />
       <h2>Home {count}</h2>
       <button onClick={() => dispatch(increment())}>Click</button>
       <div>
-       {
-        data?.data?.products?.map((product:IProduct)=>(
+        {data?.data?.products?.map((product: IProduct) => (
           <div key={product.id}>
             <img src={product.image[0]} alt={product.title} />
             <h3>{product.title}</h3>
           </div>
-        ))
-       }
+        ))}
       </div>
     </div>
   );
