b0f61f864e232579f4d256b40f425224d7f5ab64
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-22 04:29:28 +0500

    merged

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,14 @@ import { RootState } from "@/redux";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "@/redux/features/counter-slice";
-import { useGetProductsQuery } from "@/redux/api/product-api";
 import { IProduct } from "@/types";
 import Hero from "./Hero";
+import { useGetProductsQuery } from "../../redux/api/product-api";
 
 const Home = () => {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+  const {data} = useGetProductsQuery({})
 
   return (
     <div>
