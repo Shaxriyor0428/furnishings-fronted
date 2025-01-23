0e7dd57ebb98317719614117cf58500a322dcce0
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-23 08:58:27 +0500

    not found fixed

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,8 +1,12 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section className="h-screen w-full flex flex-col justify-center items-center  text-gray-600">
       <div className="text-center">
