090d628d99428594c972205449b687dbd4d454df
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-23 13:20:32 +0500

    fixed not found

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
-    <section className="h-screen w-full flex flex-col justify-center items-center bg-blue-700 text-white">
+    <section className="h-screen w-full flex flex-col justify-center items-center text-gray-600">
       <div className="text-center">
         <h1 className="text-9xl font-extrabold drop-shadow-2xl">404</h1>
         <h2 className="text-4xl font-bold mt-4 drop-shadow-2xl">
@@ -15,7 +15,7 @@ const NotFoundPage = () => {
         </p>
         <button
           onClick={() => navigate("/")}
-          className="mt-6 inline-block bg-white text-blue-600 font-semibold px-6 py-3 rounded-md shadow-md hover:bg-gray-100 transition duration-300"
+          className="mt-6 inline-block bg-white text-bg-primary font-semibold px-6 py-3 rounded-md shadow-md hover:bg-gray-100 transition duration-300"
         >
           Go Back to Home
         </button>
