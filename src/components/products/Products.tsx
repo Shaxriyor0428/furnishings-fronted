2a409aa2e7af8194d6cb92b237bc887539b6c999
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-24 11:54:34 +0500

    fixed product

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,20 +1,27 @@
 import { memo } from "react";
 import { IGetResponseProducts, IProduct } from "../../types";
-
+import { IoCartOutline } from "react-icons/io5";
+import { IoMdHeartEmpty } from "react-icons/io";
 const Products = ({ data }: { data: IGetResponseProducts }) => {
   const productItems = data?.data?.products.map((product: IProduct) => (
     <div
       key={product.id}
-      className="relative overflow-hidden rounded-lg shadow-md"
+      className="relative overflow-hidden group  rounded-lg shadow-md"
     >
-      <div className="relative w-full h-[301px] max-[620px]:h-[240px]  max-[450px]:h-[200px]">
+      <div className="relative w-full overflow-hidden h-[301px] max-[620px]:h-[240px]  max-[450px]:h-[200px]">
         <img
-          className="w-full h-full bg-no-repeat bg-center bg-cover hover:scale-[1.02] duration-300"
+          className="w-full h-full bg-no-repeat bg-center bg-cover group-hover:scale-[1.02] duration-300"
           src={`${import.meta.env.VITE_BASE_IMAGE_URL}${product.images[0]}`}
           alt={product.name}
         />
       </div>
 
+      <button className="hover:bg-slate-200  dark:text-black absolute top-1 md:top-2 right-1 md:right-[-30px] duration-300 group-hover:right-2 w-[30px] h-[30px] bg-white rounded-full flex items-center justify-center text-[20px]">
+        <IoMdHeartEmpty className="text-xl" />
+      </button>
+      <button className="hover:bg-slate-200 dark:text-black absolute top-10 md:top-11 right-1 md:right-[-30px] delay-100 duration-300 group-hover:right-2 w-[30px] h-[30px] bg-white rounded-full flex items-center justify-center text-[20px]">
+        <IoCartOutline className=" text-xl" />
+      </button>
       <div className="py-4 px-4 bg-[#F4F5F7] dark:bg-zinc-800 transition-colors duration-300">
         <h2
           title={product.name}
