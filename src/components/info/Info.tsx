2bc75dfc8db92f1de0146467178120df16d6dfc4
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-01-29 17:24:25 +0500

    fixed info

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -5,41 +5,41 @@ import c_support from "/assets/customer-support.png";
 
 const Info = () => {
   return (
-    <div className="bg-[#faf3ea] py-16 mt-16">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-4 max-sm:grid-cols-2  gap-8 text-center">
-          <div className="flex flex-col items-center">
-            <img src={trophy} alt="High Quality" className="w-14 h-14 mb-3" />
-            <h4 className="text-lg font-semibold">High Quality</h4>
-            <p className="text-sm text-gray-600">Crafted from top materials</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src={warranty}
-              alt="Warranty Protection"
-              className="w-14 h-14 mb-3"
-            />
-            <h4 className="text-lg font-semibold">Warranty Protection</h4>
-            <p className="text-sm text-gray-600">Over 2 years</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src={shipping}
-              alt="Free Shipping"
-              className="w-14 h-14 mb-3"
-            />
-            <h4 className="text-lg font-semibold">Free Shipping</h4>
-            <p className="text-sm text-gray-600">Order over $150</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src={c_support}
-              alt="24/7 Support"
-              className="w-14 h-14 mb-3"
-            />
-            <h4 className="text-lg font-semibold">24 / 7 Support</h4>
-            <p className="text-sm text-gray-600">Dedicated support</p>
-          </div>
+    <div className="bg-[#FAF3EA] dark:bg-zinc-800 font-poppins mt-20 py-10">
+      <div className="container py-8 max-lg:py-4 max-md:my-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {[
+            {
+              img: trophy,
+              title: "High Quality",
+              desc: "Crafted from top materials",
+            },
+            {
+              img: warranty,
+              title: "Warranty Protection",
+              desc: "Over 2 years",
+            },
+            { img: shipping, title: "Free Shipping", desc: "Order over $150" },
+            {
+              img: c_support,
+              title: "24 / 7 Support",
+              desc: "Dedicated support",
+            },
+          ].map((item, index) => (
+            <div key={index} className="flex items-center gap-4">
+              <div className="w-[60px] h-[60px] grid place-items-center dark:bg-white px-2 rounded-xl">
+                <img src={item.img} alt={item.title} className="w-10 h-10" />
+              </div>
+              <div>
+                <p className="text-[#242424] dark:text-white font-semibold text-lg lg:text-xl">
+                  {item.title}
+                </p>
+                <p className="text-[#898989] font-medium text-sm lg:text-base">
+                  {item.desc}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
