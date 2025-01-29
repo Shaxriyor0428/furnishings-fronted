d5563b07936d00a4343dc30a1c1e535958f09a5c
Jakhongirbey <q.jakhongirbey@gmail.com>
2025-01-29 11:27:03 +0500

    Added dark mode to info

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -4,16 +4,16 @@ import shipping from "@/../public/assets/shipping.png";
 import c_support from "@/../public/assets/customer-support.png";
 
 const Info = () => {
-    return (
-        <div className=" bg-[#FAF3EA] font-poppins mt-20">
-            <div className=" container">
-                <div className=" flex justify-between h-[270px]">
+    return (        
+        <div className=" bg-[#FAF3EA] dark:bg-zinc-800 font-poppins mt-20">
+            <div className="container">
+                <div className="flex justify-between h-[270px]">
                     <div className="flex gap-[10px] justify-center items-center">
-                        <div className="w-[60px] h-[60px] grid place-items-center">
+                        <div className="w-[60px] h-[60px] grid place-items-center dark:bg-white px-1 rounded-2xl">
                             <img src={trophy} alt="" className="" />
                         </div>
                         <div className="flex flex-col gap-[2px] h-[70px]">
-                            <p className="h-[38px] text-[#242424] font-semibold text-[25px] leading-[37.5px]">
+                            <p className="h-[38px] text-[#242424] dark:text-white font-semibold text-[25px] leading-[37.5px]">
                                 High Quality
                             </p>
                             <p className="h-[30px] text-[#898989] font-medium text-[20px] leading-[30px]">
@@ -22,11 +22,11 @@ const Info = () => {
                         </div>
                     </div>
                     <div className="flex gap-[10px] items-center">
-                        <div className="w-[60px] h-[60px] grid place-items-center">
+                        <div className="w-[60px] h-[60px] grid place-items-center dark:bg-white px-1 rounded-2xl">
                             <img src={warranty} alt="" className="" />
                         </div>
                         <div className="flex flex-col gap-[2px] h-[70px]">
-                            <p className="h-[38px] text-[#242424] font-semibold text-[25px] leading-[37.5px]">
+                            <p className="h-[38px] text-[#242424] dark:text-white font-semibold text-[25px] leading-[37.5px]">
                                 Warranty Protection
                             </p>
                             <p className="h-[30px] text-[#898989] font-medium text-[20px] leading-[30px]">
@@ -35,11 +35,11 @@ const Info = () => {
                         </div>
                     </div>
                     <div className="flex gap-[10px] items-center">
-                        <div className="w-[60px] h-[60px] grid place-items-center">
+                        <div className="w-[60px] h-[60px] grid place-items-center dark:bg-white px-1 rounded-2xl">
                             <img src={shipping} alt="" className="" />
                         </div>
                         <div className="flex flex-col gap-[2px] h-[70px]">
-                            <p className="h-[38px] text-[#242424] font-semibold text-[25px] leading-[37.5px]">
+                            <p className="h-[38px] text-[#242424] dark:text-white font-semibold text-[25px] leading-[37.5px]">
                                 Free Shipping
                             </p>
                             <p className="h-[30px] text-[#898989] font-medium text-[20px] leading-[30px]">
@@ -48,11 +48,11 @@ const Info = () => {
                         </div>
                     </div>
                     <div className="flex gap-[10px] items-center">
-                        <div className="w-[60px] h-[60px] grid place-items-center">
+                        <div className="w-[60px] h-[60px] grid place-items-center dark:bg-white px-1 rounded-2xl">
                             <img src={c_support} alt="" className="" />
                         </div>
                         <div className="flex flex-col gap-[2px] h-[70px]">
-                            <p className="h-[38px] text-[#242424] font-semibold text-[25px] leading-[37.5px]">
+                            <p className="h-[38px] text-[#242424] dark:text-white font-semibold text-[25px] leading-[37.5px]">
                                 24 / 7 Support
                             </p>
                             <p className="h-[30px] text-[#898989] font-medium text-[20px] leading-[30px]">
