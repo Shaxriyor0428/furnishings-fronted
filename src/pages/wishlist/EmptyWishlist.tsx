6252f620ea2c0f28ac84a40f3746d4a253b6aeae
Shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2025-02-01 21:11:35 +0500

    fixed contact

--- src/pages/wishlist/EmptyWishlist.tsx
+++ src/pages/wishlist/EmptyWishlist.tsx
@@ -1,17 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import emptyWishlistImage from "@/assets/images/wish-list.png"; 
 
 const EmptyWishlist = () => {
   const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center justify-center p-4 text-center h-[450px]">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-bg-primary mb-4">
+    <div className="flex flex-col items-center justify-center text-center p-6 sm:p-8 h-[500px] sm:h-[600px]">
+      <img
+        src={emptyWishlistImage}
+        alt="Empty Wishlist"
+        className="w-32 sm:w-48 md:w-60 mb-6 opacity-80"
+      />
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800 dark:text-white mb-2 sm:mb-4">
         Your Wishlist is Empty
       </h1>
-      <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-600 mb-6">
+
+      <p className="text-sm sm:text-base md:text-lg text-gray-600 dark:text-gray-300 mb-4 sm:mb-6 max-w-[80%]">
         Explore our collection and add your favorite items to your wishlist!
       </p>
+
       <button
-        className="px-4 sm:px-6 py-2 sm:py-3 bg-bg-primary text-white rounded-lg shadow hover:bg-yellow-700 transition duration-300"
+        className="px-3 sm:px-4 py-1 lg:text-2xl lg:font-medium sm:py-2 bg-[#B88E2F] text-white rounded-md shadow-md hover:bg-[#a07424] transition duration-300 text-sm sm:text-base"
         onClick={() => navigate("/")}
       >
         Go Home
