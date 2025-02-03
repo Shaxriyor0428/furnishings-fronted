ac711a56d47f6e2852614103404a2935e3c2d165
Umidjon1717 <umidizzatullayev1717@gmail.com>
2025-02-03 14:40:05 +0500

    contact responsive

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -45,54 +45,74 @@ const Contact = () => {
       const data = await response.json();
 
       if (data.ok) {
-        toast.success("Message sent successfully!",{position:"top-right"});
+        toast.success("Message sent successfully!", { position: "top-right" });
         if (nameRef.current) nameRef.current.value = "";
         if (emailRef.current) emailRef.current.value = "";
         if (subjectRef.current) subjectRef.current.value = "";
         if (messageRef.current) messageRef.current.value = "";
       } else {
-        toast.error("Failed to send message. Please try again.",{position:"top-right"});
+        toast.error("Failed to send message. Please try again.", {
+          position: "top-right",
+        });
       }
     } catch (error) {
       console.error("Error sending message:", error);
-      toast.error("Failed to send message. Please try again.",{position:"top-right"});
+      toast.error("Failed to send message. Please try again.", {
+        position: "top-right",
+      });
     }
   };
 
   return (
     <div className="container">
-      <div className="mt-16 text-center">
-        <h2 className="text-[36px] font-[600]">Get In Touch With Us</h2>
-        <p className="text-[#9F9F9F]">
+      <div className="mt-16 text-center px-4">
+        <h2 className="text-2xl md:text-4xl lg:text-[44px] font-semibold">
+          Get In Touch With Us
+        </h2>
+        <p className="text-gray-500 text-sm md:text-base max-w-lg mx-auto">
           For more information about our products & services, feel free to drop
           us an email. Our staff is always here to help!
         </p>
       </div>
 
-      <div className="flex mt-14 flex-col md:flex-row items-start justify-between gap-8 p-8 rounded-lg max-w-4xl mx-auto">
-        <div className="w-full md:w-1/2">
-          <p className="text-gray-600 mb-12 dark:text-gray-400">
-            <img src={address} alt="address" />
-            <strong className="text-[24px] dark:text-gray-300"> Address</strong>
-            <br /> 236 5th SE Avenue, New York NY10000, United States
+      <div className="flex mt-14 flex-col md:flex-row items-start justify-between gap-10 p-8 rounded-lg max-w-4xl mx-auto">
+        <div className="w-full md:w-1/2 space-y-8">
+          <p className="text-gray-600 dark:text-gray-400 flex items-start gap-4">
+            <img src={address} alt="address" className="w-8 h-8" />
+            <span>
+              <strong className="text-lg md:text-xl dark:text-gray-300">
+                Address
+              </strong>
+              <br />
+              236 5th SE Avenue, New York NY10000, United States
+            </span>
           </p>
-          <p className="text-gray-600 mb-12 dark:text-gray-400">
-            <img src={phone} alt="phone" />
-            <strong className="text-[24px]"> Phone</strong>
-            <br /> Mobile: +(84) 546-6789 <br /> Hotline: +(84) 456-6789
+
+          <p className="text-gray-600 dark:text-gray-400 flex items-start gap-4">
+            <img src={phone} alt="phone" className="w-8 h-8" />
+            <span>
+              <strong className="text-lg md:text-xl">Phone</strong>
+              <br />
+              Mobile: +(84) 546-6789 <br />
+              Hotline: +(84) 456-6789
+            </span>
           </p>
-          <p className="text-gray-600 mb-12 dark:text-gray-400">
-            <img src={time} alt="time" />
-            <strong className="text-[24px]"> Working Hours</strong>
-            <br /> Monday-Friday: 9:00 - 22:00 <br /> Saturday-Sunday: 9:00 -
-            21:00
+
+          <p className="text-gray-600 dark:text-gray-400 flex items-start gap-4">
+            <img src={time} alt="time" className="w-8 h-8" />
+            <span>
+              <strong className="text-lg md:text-xl">Working Hours</strong>
+              <br />
+              Monday-Friday: 9:00 - 22:00 <br />
+              Saturday-Sunday: 9:00 - 21:00
+            </span>
           </p>
         </div>
 
-        <div className="w-full md:w-1/2 bg-white dark:bg-zinc-900 p-6 rounded-lg">
+        <div className="w-full md:w-1/2 bg-white dark:bg-zinc-900 pl-8 md:pl-0 rounded-lg">
           <form
             onSubmit={handleSubmit}
-            className="max-w-lg mx-auto bg-white dark:bg-zinc-900 p-6 rounded-lg space-y-4"
+            className="max-w-lg mx-auto bg-white dark:bg-zinc-900 rounded-lg space-y-4"
           >
             <div className="flex flex-col">
               <label
